eadd9dfdbe1d5fffe3e0cb2a06fedbe5ed4dc3f8
JOHNFLEURIMOND <johnnyxxboston@gmail.com>
2019-01-28 09:14:22 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {
+  Component
+} from 'react';
+import Todo from './components/Todo';
+import Addtodo from './components/Addtodo';
+import Header from './components/Header';
+import axios from 'axios';
+
+
 import './App.css';
 
 class App extends Component {
+  state = {
+    todos: []
+  }
+
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
+      .then(res => this.setState({
+        todos: res.data
+      }))
+  }
+
+  //toggle complete
+  markComplete = (id) => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo;
+      })
+    });
+  }
+
+  // Delete Todo
+  deleteTodo = (id) => {
+    axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+      .then(res => this.setState({
+        todos: [...this.state.todos.filter(todo => todo.id !== id)]
+      }));
+  }
+
+  // Add Todo
+  Addtodo = (
+    title
+  ) => {
+    axios.post("https://jsonplaceholder.typicode.com/todos?", {
+        title,
+        completed: false
+      })
+      .then(res => this.setState({
+        todos: [...this.state.todos, res.data]
+      }));
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+    return ( <
+      div className = "App" >
+      <
+      div className = "container" >
+      <
+      Header / >
+      <
+      Addtodo Addtodo = {
+        this.Addtodo
+      }
+      / > <
+      Todo todos = {
+        this.state.todos
+      }
+      markComplete = {
+        this.markComplete
+      }
+      deleteTodo = {
+        this.deleteTodo
+      }
+      / > < /div > < /div >
     );
   }
 }
---
+++ src/components/about.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function About() {
+  return ( <
+    React.Fragment >
+    <
+    h1 > About < /h1> <
+    p > This is as done by John Fleurimond < /p> < /
+    React.Fragment >
+  )
+}
+
+
+export default About;
