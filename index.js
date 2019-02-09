396426e5c67ddbaca40a8418477ef0284324ad9c
JOHNFLEURIMOND <johnnyxxboston@gmail.com>
2019-02-09 00:36:13 -0500

    make it better

--- index.js
+++ index.js
@@ -55,14 +55,14 @@ app.post('/api/form', (req, res) => {
       if (err) {
         return console.log(err);
       }
-      
+
       console.log('Message sent: %s', info.message);
       console.log('Message URL: %s', nodemailer.getTestMessage(info));
     });
   });
 });
 
-const port = process.env.PORT || 7800;
+const port = process.env.PORT;
 
 app.listen(port, () => {
   console.log('Whatever is clever, quit never!' + port);
