1a434f156b20cc14f882d497fbfad7aa06b4aca2
JOHNFLEURIMOND <johnnyxxboston@gmail.com>
2019-03-07 15:43:33 -0500

    First commit.

---
+++ build/precache-manifest.eda96b521027f0ee5000e614503a46c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "50d2cc3a14457cca869e",
+    "url": "/static/css/main.32c5eb4a.chunk.css"
+  },
+  {
+    "revision": "50d2cc3a14457cca869e",
+    "url": "/static/js/main.50d2cc3a.chunk.js"
+  },
+  {
+    "revision": "8c742eca5ce10d1b32f6",
+    "url": "/static/js/1.8c742eca.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "064682b800ac6bb6db62327592303ffd",
+    "url": "/index.html"
+  }
+];
